0e7ff2634a0df85466086d5f2fafeba5bcda22e8
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2021-01-06 13:21:11 +0700

    add NIM to footer

--- resources/views/layouts/_partials/footer.blade.php
+++ resources/views/layouts/_partials/footer.blade.php
@@ -3,6 +3,6 @@
         Copyright &copy; <script>
             document.write(new Date().getFullYear());
 
-        </script> Webkami Team
+        </script> LB01-WebProg Webkami Team ( 2201731106 , 2201760711 , 2201765246 , 2201829666 , 2201781994 )
     </p>
 </footer>
